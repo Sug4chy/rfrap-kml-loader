c4d2aadd0315f832a50795feb19ef1bae923c6c8
sug4chy <sb.9226981433@gmail.com>
2024-10-20 23:35:01 +0500

    Clean code in XmlReadingConfiguration class

--- src/main/java/ru/sug4chy/rfrapkmloader/configuration/XmlReadingConfiguration.java
+++ src/main/java/ru/sug4chy/rfrapkmloader/configuration/XmlReadingConfiguration.java
@@ -1,19 +1,18 @@
 package ru.sug4chy.rfrapkmloader.configuration;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.oxm.xstream.XStreamMarshaller;
-import ru.sug4chy.rfrapkmloader.model.xml.*;
+import ru.sug4chy.rfrapkmloader.model.xml.PlacemarkXmlModel;
+import ru.sug4chy.rfrapkmloader.model.xml.PointXmlModel;
+
 import java.util.Map;
 
 @Configuration
-@EnableBatchProcessing
 public class XmlReadingConfiguration {
 
     @Value("classpath:${rfrap-kml-loader.kml-file-name}")
@@ -29,7 +28,7 @@ public class XmlReadingConfiguration {
 
         ));
 
-        marshaller.getXStream().allowTypes(new Class[]{
+        marshaller.getXStream().allowTypes(new Class[] {
                 PlacemarkXmlModel.class,
                 PointXmlModel.class
         });
